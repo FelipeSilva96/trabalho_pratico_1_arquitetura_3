05cd662d792192f58d9e6c8ed4523462f2c44f9a
Mateus Ottoni <mateus.resende.ottoni@gmail.com>
2026-05-31 07:04:40 -0300

    Update testbench tests
    Adição dos testes necessários restantes ao testbench

--- tb/tb_dm_cache.sv
+++ tb/tb_dm_cache.sv
@@ -13,6 +13,7 @@ module tb_dm_cache;
 
     /* verilator lint_off UNUSEDSIGNAL */
     bit [3:0] test_instruction_number;
+    int address;
     /* verilator lint_on UNUSEDSIGNAL */
 
     cpu_req_type    cpu_req_in;
@@ -95,7 +96,7 @@ module tb_dm_cache;
     // ---------------------------------------------------------
     
     // Task para simular uma LEITURA da CPU
-    task cpu_read(input logic [31:0] addr);
+    task cpu_read(input logic [31:0] addr, input logic show_result);
         begin
             @(posedge clk);
             cpu_req_in.valid = 1'b1; 
@@ -107,12 +108,14 @@ module tb_dm_cache;
             @(posedge clk);
             
             cpu_req_in.valid = 1'b0;
-            $display("[%0t] LEITURA CONCLUIDA | Endereco: %h | Dado Lido: %h", $time, addr, cpu_res_out.data);
+            if (show_result) begin
+                $display("[%0t] LEITURA CONCLUIDA | Endereco: %h | Dado Lido: %h", $time, addr, cpu_res_out.data);
+            end
         end
     endtask
 
     // Task para simular uma ESCRITA da CPU
-    task cpu_write(input logic [31:0] addr, input logic [31:0] data);
+    task cpu_write(input logic [31:0] addr, input logic [31:0] data, input logic show_result);
         begin
             @(posedge clk);
             cpu_req_in.valid = 1'b1;
@@ -124,7 +127,9 @@ module tb_dm_cache;
             @(posedge clk);
             
             cpu_req_in.valid = 1'b0;
-            $display("[%0t] ESCRITA CONCLUIDA | Endereco: %h | Dado Escrito: %h", $time, addr, data);
+            if (show_result) begin
+                $display("[%0t] ESCRITA CONCLUIDA | Endereco: %h | Dado Escrito: %h", $time, addr, data);
+            end
         end
     endtask
 
@@ -160,32 +165,81 @@ module tb_dm_cache;
         #20 rst = 0; // Libera o reset após 20ns
         $display("[%0t] Reset liberado. Cache inicializada.", $time);
 
+        //  Seria interessante um teste para percorrer a cache e relatar se houver
+        // algumas linha diferente de 0, mas isso não é possível pela forma
+        // como é feita a modularização, em que não se tem acesso a toda a cache diretamente
+
         // --- INÍCIO DOS CENÁRIOS DE TESTE ---
 
-        $display("\n\n--- Teste 7.1: Leitura de endereco vazio (Miss) ---");
+        $display("\n\n--- Teste 1.1: Leitura de endereco vazio (Miss) ---");
         // Vai ler o endereço. Como está vazio, vai na memória buscar. 
         // A memória deve devolver a palavra correspondente ao final do bloco (AAAAAAAA, BBBBBBBB, etc).
         test_instruction_number = 4'd1;
-        cpu_read(32'h0000_1004); 
+        cpu_read(32'h0000_1004, 1'b1); 
 
-        $display("\n\n--- Teste 7.1: Leitura do mesmo endereco (Hit) ---");
+        $display("\n\n--- Teste 1.2: Leitura do mesmo endereco (Hit) ---");
         // O dado agora deve estar na cache, resolvendo em 1 ou 2 ciclos sem ir na memória.
         test_instruction_number = 4'd2;
-        cpu_read(32'h0000_1004);
+        cpu_read(32'h0000_1004, 1'b1);
 
-        $display("\n\n--- Teste 7.2: Escrita no mesmo endereco (Hit Write) ---");
+        $display("\n\n--- Teste 2.1: Escrita no mesmo endereco (Hit Write) ---");
         // Vai sobrescrever a palavra que estava lá pelo valor DEADBEEF e marcar o Dirty bit como 1.
         test_instruction_number = 4'd3;
-        cpu_write(32'h0000_1004, 32'hDEADBEEF);
+        cpu_write(32'h0000_1004, 32'hDEADBEEF, 1'b1);
 
-        $display("\n\n--- Teste 7.3: Escrita em endereco ocupado (Substituicao) ---");
-        test_instruction_number = 4'd4;
+        $display("\n\n--- Teste 2.2: Escrita em endereço com dirty bit 1 (Política Write-Back)---");
+        // Endereço tem mesmo índice que o anterior, mas o tag é diferente
+        cpu_write(32'h0001_1004, 32'h0000000F, 1'b1);
 
-        $display("\n\n--- Teste 7.4: Consistencia em acesso ---");
+        $display("\n\n--- Teste 2.3: Escrita em endereco diferente (Miss Write)---");
+        // Endereço tem índice não salvo na cache
+        cpu_write(32'h0000_1014, 32'h0000001F, 1'b1);
+
+        $display("\n\n--- Teste 3.1: Consistencia em sequencia de operacoes ---");
         test_instruction_number = 4'd5;
+        //  Aqui provamos a consistência de dados em um endereço
+        // salvando um valor a ele, substituindo sua presença na cache
+        // com um endereço de mesmo índice mas tag diferente, e então realizando
+        // a leitura novamente para verificar que o valor permanece
+        cpu_read(32'h0000_1010, 1'b1);
+        cpu_write(32'h0000_1010, 32'h0000002F, 1'b1);
+        cpu_read(32'h0000_1010, 1'b1);
+        cpu_read(32'h0000_3010, 1'b1);
+        cpu_read(32'h0000_1010, 1'b1);
 
-        $display("\n\n--- Teste 7.5: Casos Limite ---");
+        $display("\n\n--- Teste 3.2: Consistencia em acesso ao mesmo endereco ---");
+        cpu_write(32'h0000_1020, 32'h0000003F, 1'b1);
+        for (int i = 0; i < 4; i++) begin
+            cpu_read(32'h0000_1020, 1'b1);
+        end
+
+        $display("\n\n--- Teste 3.3: Acesso em enderecos com conflito de indice ---");
+        cpu_write(32'h0000_1030, 32'hFF0000FF, 1'b1);
+        cpu_write(32'h0001_1030, 32'h00FFFF00, 1'b1);
+        cpu_read(32'h0000_1030, 1'b1);
+        // Endereço tem mesmo índice que o anterior, mas o tag é diferente
+        cpu_read(32'h0001_1030, 1'b1);
+
+        $display("\n\n--- Teste 4: Enderecos Extremos---");
         test_instruction_number = 4'd6;
+        // Escrito no primeiro e último endereço da cache
+        cpu_write(32'h0000_0000, 32'h00000001, 1'b1);
+        cpu_read(32'h0000_0000, 1'b1);
+        cpu_write(32'hFFFF_FFFF, 32'hFFFFFFFF, 1'b1);
+        cpu_read(32'hFFFF_FFFF, 1'b1);
+
+        $display("\n\n--- Teste 5:Preenchimento da cache ---");
+        test_instruction_number = 4'd4;
+        $display("\n Preenchendo a cache \n");
+        for (int i = 0; i < CACHE_LINES; i++) begin
+            address = 32'h0000_0000;
+            address = address + (i*16);
+            cpu_write(address, 32'h00000001, 1'b0);
+            if ((i+1)%128 == 0) begin
+                $display(" Preenchido %d enderecos", i+1);
+            end
+        end
+        $display("\n Cache preenchida");
 
         // Finaliza a simulação
         #50;
